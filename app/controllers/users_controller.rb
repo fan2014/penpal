b48aedf6c8c08a8f99263b53c7aedf9e9984fc08
Fan <fzheng3@wisc.edu>
2014-08-15 22:03:44 -0500

    Done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
-  def show
+  def edit
+    if signed_in?
+      @user = current_user
+    else
+      render 'sessions/new'
+    end
+  end
+  
+  def update
+    if signed_in?
+      user = current_user
+      user.name = params[:user][:name]
+      user.password = params[:user][:password]
+      user.password_confirmation = params[:user][:password_confirmation]
+      
+      if user.save
+        flash[:success]="name/password has been saved"
+        
+        redirect_to mails_path
+      else
+        flash[:error]="invalid new password, make sure your password is more than 8 characters"
+        redirect_to edit_user_path(user)
+      end
+    else
+      render 'sessions/new'
+    end
   end
 end
